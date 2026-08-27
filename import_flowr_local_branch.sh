d707624ecee2cef8465c8b1b8e4d9a3500c19f17
Mark <>
2026-08-27 18:56:34 +0200

    fix: add jank to make local import work (for now)
    also adds todos telling me to remove said jank

--- import_flowr_local_branch.sh
+++ import_flowr_local_branch.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# TODO: todos in this script
+#       + remove dependencies commonmark, gray-matter, smol-toml
+#         (they should be dependencies of flowr, not the eval;
+#          they are required due to flowr main todo from below)
+
 # get flowr repo if it's not here already
 # (if this fails, assume flowr repo is already cloned)
 git clone https://github.com/flowr-analysis/flowr
@@ -7,6 +12,15 @@ set -e
 mkdir -p vendor
 
 cd flowr
+
+# build flowr - TODO: REMOVE once branch is up-to-date
+# this uses branch `main`, which provides `dist/src/index.js`
+# (possibly among other files missing from the feature branch).
+# this means we package a mix of `main` and the (outdated)
+# feature branch. this somehow works but it's just so wrong :(
+npm i
+npm run build:dev
+
 # TODO: switch to my actual branch, don't copy script
 COPY1="$(cat ./scripts/stage-library.ts)"
 git switch 174-add-type-inference
