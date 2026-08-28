b078c6c5299afdfe79521a70ae9a66bae8895aa2
Mark <>
2026-08-28 15:29:11 +0200

    feat: infer types for every `<var> #type=<name>#`
    for every line in an eval R script which has the
    following format, the type of that variable is
    inferred. For each script, a mapping from
    <typename> to the inferred type is stored.
    Line format and example:
    
    ```
    <optional whitespace><variable> #type=<typename>#
      x #type=first_usage_of_x#
    ```

---
+++ eval/flow01.R
@@ -0,0 +1,11 @@
+if (0) {
+  x <- 0
+} else {
+  x <- ""
+}
+x #type=x#
+if (is.numeric(x)) {
+  x #type=num#
+} else {
+  x #type=str#
+}
--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,49 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
-import { spawn } from 'child_process'
-// import { FlowrAnalyzerBuilder } from '@eagleoutice/flowr/project/flowr-analyzer-builder';
-// import { jsonReplacer } from '@eagleoutice/flowr/util/json';
-// import { fileProtocol } from '@eagleoutice/flowr/r-bridge/retriever.js';
-// import { log, LogLevel } from '@eagleoutice/flowr/util/log';
 
-import { query } from './query';
-import { arrayBuffer } from 'stream/consumers';
+import { AbstractFlowr } from './abstract_flowr';
 
-async function main(folder: string, outputFile: string) {
-   // log.updateSettings(s => {
-   //    s.settings.minLevel = LogLevel.Fatal;
-   // });
-
-   // const analyzer = await new FlowrAnalyzerBuilder()
-   //    .setEngine('tree-sitter')
-   //    .build();
-   // analyzer.addRequest(fileProtocol + folder);
+async function main(folder: string) {
+   const directory = path.resolve(folder);
+   let flowr = new AbstractFlowr(directory);
    try {
-      const file = "test.R";
-      const target = folder.startsWith('/') ? path.join(folder, file) : path.join("..", folder, file);
-      const query_string = JSON.stringify(query).replaceAll('"', '\\"');
-      const process = spawn(
-         'npm',
-         ['run', 'main', '--', '--execute', `:query* "${query_string}" "file://${target}"`],
-         { 'cwd': './flowr/', stdio: 'pipe' }
-      );
-      const resultBytes = await arrayBuffer(process.stdout);
-      await new Promise((resolve, reject) => {
-         process.on('exit', (d) => resolve(d));
-         process.on('error', (d) => reject(d));
-      });
-      const resultString = new TextDecoder().decode(resultBytes);
-      const results = JSON.parse(resultString);
-      // const results = await analyzer.query(query);
-      // const resultString = JSON.stringify(results, jsonReplacer, 2);
-      fs.writeFileSync(outputFile, resultString, 'utf-8');
-      console.log(`Results written to ${outputFile} 
-   * ${Object.entries(results).length} results (serialized: ${resultString.length} chars)`);
+      for (const file of await fs.readdir(directory)) {
+         console.log("=>", file);
+         let query: { type: string, criteria: string[] } = { type: 'datatype', criteria: [] };
+         let typenames: Map<string, string> = new Map();
+         const contents = await fs.readFile(path.join(directory, file), { encoding: 'utf-8' });
+         const lines = contents.split('\n');
+         for (let line = 0; line < lines.length; line++) {
+            const parts = lines[line].trim().split(' ');
+            if (parts.length == 2 && parts[1].startsWith('#type=') && parts[1].endsWith('#')) {
+               const varname = parts[0];
+               const typename = parts[1].substring(6, parts[1].length - 1);
+               const criterion = `${line+1}@${varname}`;
+               query.criteria.push(criterion);
+               typenames.set(criterion, typename);
+            }
+         }
+         let [results, resultString] = await flowr.query(file, [query]);
+         const inferredTypes = new Map();
+         for (const [criterion, typename] of typenames) {
+            const resultType = results.datatype?.inferredTypes?.[criterion];
+            inferredTypes.set(typename, resultType);
+         }
+         console.log(inferredTypes);
+         console.log(resultString);
+      }
    } finally {
-      // analyzer.close();
+      flowr.close();
    }
 }
-if(process.argv.length < 4) {
-   console.error('Usage: ts-node src/main.ts <folder> <output-file>');
+if(process.argv.length < 3) {
+   console.error('Usage: `ts-node src/main.ts <folder>`, where `<folder>` is usually `eval`');
    process.exit(1);
 }
 
 const folder = process.argv[2];
-const outputFile = process.argv[3];
 
-void main(folder, outputFile).catch(err => {
-   console.error('Error during analysis:', err);
+void main(folder).catch(err => {
+   console.error('Error during eval:', err);
    process.exit(1);
 });
